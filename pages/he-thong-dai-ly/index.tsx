2099247a8d13c4accbefc47f5c232bafc4d26026
Alan Nguyen <alan.nguyen@setel.com>
2022-10-17 17:31:03 +0700

    Done he thong dai ly

--- pages/he-thong-dai-ly/index.tsx
+++ pages/he-thong-dai-ly/index.tsx
@@ -2,73 +2,26 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import styles from './styles.module.scss';
 import { AiTwotoneEnvironment, AiFillPhone } from "react-icons/ai";
+import data from './data.json';
 export default function Introduction() {
     return (
         <Container className={styles.systemStore}>
             <h2 className="title-header">CHUỖI HỆ THỐNG THẨM MỸ VIỆN DOCTOR LIGI CLINIC</h2>
             <Row>
-                <Col xs={12} lg={4} className='p-3'>
-                    <div className={styles.image}>
-                        <div>
-                            <div>Hệ thống tại</div>
-                            <div>HÀ NỘI</div>
-                        </div>
-                    </div>
-                    <div className={styles.address}><AiTwotoneEnvironment /> 299 Kim Mã - Ba Đình - Hà Nội</div>
-                    <div className={styles.telephone}><AiFillPhone /> 0862248237</div>
-                </Col>
-                <Col xs={12} lg={4} className='p-3'>
-                    <div className={styles.image}>
-                        <div>
-                            <div>Hệ thống tại</div>
-                            <div>HÀ NỘI</div>
-                        </div>
-                    </div>
-                    <div className={styles.address}><AiTwotoneEnvironment /> 299 Kim Mã - Ba Đình - Hà Nội</div>
-                    <div className={styles.telephone}><AiFillPhone /> 0862248237</div>
-                </Col>
-                <Col xs={12} lg={4} className='p-3'>
-                    <div className={styles.image}>
-                        <div>
-                            <div>Hệ thống tại</div>
-                            <div>HÀ NỘI</div>
-                        </div>
-                    </div>
-                    <div className={styles.address}><AiTwotoneEnvironment /> 299 Kim Mã - Ba Đình - Hà Nội</div>
-                    <div className={styles.telephone}><AiFillPhone /> 0862248237</div>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12} lg={4} className='p-3'>
-                    <div className={styles.image}>
-                        <div>
-                            <div>Hệ thống tại</div>
-                            <div>HÀ NỘI</div>
-                        </div>
-                    </div>
-                    <div className={styles.address}><AiTwotoneEnvironment /> 299 Kim Mã - Ba Đình - Hà Nội</div>
-                    <div className={styles.telephone}><AiFillPhone /> 0862248237</div>
-                </Col>
-                <Col xs={12} lg={4} className='p-3'>
-                    <div className={styles.image}>
-                        <div>
-                            <div>Hệ thống tại</div>
-                            <div>HÀ NỘI</div>
-                        </div>
-                    </div>
-                    <div className={styles.address}><AiTwotoneEnvironment /> 299 Kim Mã - Ba Đình - Hà Nội</div>
-                    <div className={styles.telephone}><AiFillPhone /> 0862248237</div>
-                </Col>
-                <Col xs={12} lg={4} className='p-3'>
-                    <div className={styles.image}>
-                        <div>
-                            <div>Hệ thống tại</div>
-                            <div>HÀ NỘI</div>
-                        </div>
-                    </div>
-                    <div className={styles.address}><AiTwotoneEnvironment /> 299 Kim Mã - Ba Đình - Hà Nội</div>
-                    <div className={styles.telephone}><AiFillPhone /> 0862248237</div>
-                </Col>
+                {
+                    data.agentSystem.map((value, index) => (
+                        <Col xs={12} lg={4} className='p-3' key={index}>
+                            <div className={styles.image} style={{ backgroundImage: `url(${value.image})` }} >
+                                <div>
+                                    <div>Hệ thống tại</div>
+                                    <div>{value.province}</div>
+                                </div>
+                            </div>
+                            <div className={styles.address}><AiTwotoneEnvironment /> {value.address}</div>
+                            <div className={styles.telephone}><AiFillPhone /> {value.telephone}</div>
+                        </Col>
+                    ))
+                }
             </Row>
         </Container>
     )
